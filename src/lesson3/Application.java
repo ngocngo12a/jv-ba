e063993f20d2d8dc09722f8ecd048689c24cac2d
ngocngo12a <milonasun@gmail.com>
2020-07-09 16:38:47 +0700

    new

--- src/lesson3/Application.java
+++ src/lesson3/Application.java
@@ -1,5 +1,6 @@
 package lesson3;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -10,148 +11,23 @@ public class Application {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("-----------CHỨC NĂNG----------");
-        System.out.println("1. Tìm kiếm laptop theo hãng sản xuất");
-        System.out.println("2. Tìm kiếm laptop theo khoảng giá");
-        System.out.println("3. Tìm kiếm laptop theo loại ổ cứng và hãng sản xuất");
-        System.out.println("4. Tìm kiếm laptop theo kích thước màn hình");
-        System.out.println("5. Tìm kiếm theo Ram laptop");
-        System.out.println("6. Tìm kiếm theo CPU ");
-        System.out.println("7. Tìm kiếm theo loại máy tính");
-        System.out.println("8. Tìm kiếm theo card ");
-        System.out.println("9. Tìm kiếm danh sách laptop bán chạy nhất");
-
+        System.out.println("Bấm phím 1 để tìm kiếm laptop");
         do {
             System.out.println("\nNhập lựa chọn:");
             int option = scanner.nextInt();
             scanner.nextLine();
             switch (option) {
                 case 1:
-                    System.out.println("Nhập vào tên hãng :");
-                    String maker = scanner.nextLine();
-                    List<LaptopModel> laptopModels = laptopService.findAllByMaker(maker.trim().toUpperCase());
+
+                    List<LaptopModel> laptopModels = new ArrayList<>();
+                    laptopModels = laptopService.searchLaptop("ACER",15000000.0f,20000000.0f,null,null,null,null);
 
                     if (laptopModels == null || laptopModels.isEmpty()) {
-                        System.out.println("Không tìm thấy ");
-                    } else {
-                        for (LaptopModel laptopModel : laptopModels) {
-                            System.out.println("Tên sản phẩm: " + laptopModel.getName() + "  -  Mức giá: " + laptopModel.getPrice() + "VND");
-                        }
-                    }
-                    break;
-
-                case 2:
-                    System.out.println("Nhập vào khoảng giá :");
-                    System.out.println("Giá mim =: ");
-                    Float priceFrom = scanner.nextFloat();
-                    System.out.println("Giá max = ");
-                    Float priceTo = scanner.nextFloat();
-
-                    List<LaptopModel> laptopModels1;
-                    if (priceFrom == null) {
-                        priceFrom = 0.0f;
-                    } else if (priceTo == null) {
-                        priceTo = 9999999999.9f;
-                    } else if (priceFrom == null && priceTo == null) {
-                        priceFrom = 0.0f;
-                        priceTo = 9999999999.9f;
-                    }
-                    laptopModels1 = laptopService.findAllByPrice(priceFrom, priceTo);
-
-                    if (laptopModels1 == null || laptopModels1.isEmpty()) {
                         System.out.println("Không tìm thấy thông tin sản phẩm.");
                     } else {
-                        for (LaptopModel laptopModel : laptopModels1) {
-                            System.out.println("Tên sản phẩm: " + laptopModel.getName() + "  -  Mức giá: " + laptopModel.getPrice() + "VND");
+                        for (LaptopModel laptopModel : laptopModels) {
+                            System.out.println(laptopModel.getName());
                         }
-                    }
-                    break;
-                case 3:
-                    System.out.println("Nhập vào tên hãng :");
-                    String makers = scanner.nextLine();
-                    System.out.println("Nhập vào dung lượng ổ cứng : ");
-                    String gb = scanner.nextLine();
-
-                    List<LaptopModel> laptopModels2 = laptopService.findAllByMakerAndSSD(makers.trim().toUpperCase(), gb);
-
-                    if (laptopModels2 == null || laptopModels2.isEmpty()) {
-                        System.out.println("Không tìm thấy thông tin ");
-                    } else {
-                        for (LaptopModel laptopModel : laptopModels2) {
-                            String priceOfProduct = laptopModel.getPrice().toString();
-                            System.out.println("Tên sản phẩm: " + laptopModel.getName() + "  -  Mức giá: " + priceOfProduct + "VND");
-                        }
-                    }
-                    break;
-
-                case 4 :
-                    System.out.println("Nhập vào kích thước màn hình (inch) :");
-                    String Screensize = scanner.nextLine();
-                    List<LaptopModel> laptopModels3 = laptopService.findAllByScreensize(Screensize);
-                    if(laptopModels3 == null || laptopModels3.isEmpty()){
-                        System.out.println("Khong tim thấy thông tin ");
-                    }
-                    else {
-                        for(LaptopModel laptopModel : laptopModels3){
-                            System.out.println("Tên sản phẩm: " + laptopModel.getName() + "  -  Mức giá: " + laptopModel.getPrice() + "VND");
-                        }
-                    }
-                    break;
-                case 5 :
-                    System.out.println("Nhập vào Ram laptop : ");
-                    String Ram = scanner.nextLine();
-                    List<LaptopModel> laptopModels4 = laptopService.findAllByRam(Ram);
-                    if(laptopModels4 == null || laptopModels4.isEmpty()){
-                        System.out.println("Khong tim thấy thông tin ");
-                    }
-                    else {
-                        for(LaptopModel laptopModel : laptopModels4){
-                            System.out.println("Tên sản phẩm: " + laptopModel.getName() + "  -  Mức giá: " + laptopModel.getPrice() + "VND");
-                        }
-                    }
-                    break;
-                case 6 :
-                    System.out.println("Nhập vào CPU laptop : ");
-                    String CPU = scanner.nextLine();
-                    List <LaptopModel> laptopModels5 = laptopService.findAllByCPU(CPU);
-                    if(laptopModels5 == null || laptopModels5.isEmpty()){
-                        System.out.println("Khong tim thấy thông tin ");
-                    }
-                    else {
-                        for(LaptopModel laptopModel : laptopModels5){
-                            System.out.println("Tên sản phẩm: " + laptopModel.getName() + "  -  Mức giá: " + laptopModel.getPrice() + "VND");
-                        }
-                    }
-                    break;
-                case 7 :
-                    System.out.println("Nhập vào loại laptop : ");
-                    String Type = scanner.nextLine();
-                    List <LaptopModel> laptopModels6 = laptopService.findAllByType(Type);
-                    if(laptopModels6 == null || laptopModels6.isEmpty()){
-                        System.out.println("Khong tim thấy thông tin ");
-                    }
-                    else {
-                        for(LaptopModel laptopModel : laptopModels6){
-                            System.out.println("Tên sản phẩm: " + laptopModel.getName() + "  -  Mức giá: " + laptopModel.getPrice() + "VND");
-                        }
-                    }
-                    break;
-                case 8 :
-                    System.out.println("Nhập vào card laptop : ");
-                    String Card = scanner.nextLine();
-                    List <LaptopModel> laptopModels7 = laptopService.findAllByCard(Card);
-                    if(laptopModels7 == null || laptopModels7.isEmpty()){
-                        System.out.println("Khong tim thấy thông tin ");
-                    }
-                    else {
-                        for(LaptopModel laptopModel : laptopModels7){
-                            System.out.println("Tên sản phẩm: " + laptopModel.getName() + "  -  Mức giá: " + laptopModel.getPrice() + "VND");
-                        }
-                    }
-                    break;
-                case 9 :
-                    List <LaptopModel> laptopModels8 = laptopService.findAllBySold();
-                    for(LaptopModel laptopModel : laptopModels8){
-                        System.out.println("Tên sản phẩm: " + laptopModel.getName() + "  -  Mức giá: " + laptopModel.getPrice() + "VND");
                     }
             }
         }while(true);
--- src/lesson3/LaptopServices.java
+++ src/lesson3/LaptopServices.java
@@ -27,304 +27,63 @@ public class LaptopServices {
     public LaptopServices(Connection connection) {
         this.connection = connection;
     }
+    public List<LaptopModel> searchLaptop( String maker, Float fromPrice, Float toPrice, String cpu, String ram, String card, Float screeenSize) {
+        String sql = "SELECT * FROM laptop WHERE TRUE ";
+        if (maker != null) {
+            sql += " AND maker = '" + maker + "'";
+        }
+        if (fromPrice != null) {
+            sql += " AND price >= " + fromPrice;
+        }
+        if (toPrice != null) {
+            sql += " AND price <= " + toPrice;
+        }
+        if (cpu != null) {
+            sql += " AND cpu LIKE '%" + cpu + "%'";
+        }
+        if (ram != null) {
+            sql += " AND ram = '" + ram + "'";
+        }
+        if (card != null) {
+            sql += " AND card = '" + card + "'";
+        }
+        if (screeenSize != null) {
+            sql += " AND screen_size = '" + screeenSize + "'";
+        }
 
-    public List<LaptopModel> findAllByMaker(String maker) {
-        try {
-            List<LaptopModel> laptopModels = new ArrayList<>();
-            Statement statement = getConnections().createStatement();
-            ResultSet rs = statement.executeQuery("select * from laptop WHERE maker = '" + maker + "'   ");
-            while (rs.next()) {
-                LaptopModel laptopModel = new LaptopModel(
-                        rs.getInt(1),
-                        rs.getString(2),
-                        rs.getString(3),
-                        rs.getString(4),
-                        rs.getString(5),
-                        rs.getString(6),
-                        rs.getString(7),
-                        rs.getString(8),
-                        rs.getString(9),
-                        rs.getFloat(10),
-                        rs.getString(11),
-                        rs.getString(12),
-                        rs.getFloat(13),
-                        rs.getInt(14),
-                        rs.getTimestamp(15),
-                        rs.getTimestamp(16)
-                );
-                laptopModels.add(laptopModel);
-
-            }
-            return laptopModels;
-        }catch( Exception e){
-            System.out.println("Loi khi tim kiem san pham bang maker");
-        }
-        return null;
-
-    }
-    public List<LaptopModel> findAllByPrice(Float priceFrom, Float priceTo) {
-        try {
-            List<LaptopModel> laptopModels = new ArrayList<>();
-            Statement statement = getConnections().createStatement();
-            String sql = "SELECT * FROM laptop WHERE price BETWEEN '" + priceFrom + "'" + "AND '" + priceTo + "'";
-            ResultSet resultSet = statement.executeQuery(sql);
-            while (resultSet.next()) {
-                LaptopModel laptopModel = new LaptopModel(
-                        resultSet.getInt(1),
-                        resultSet.getString(2),
-                        resultSet.getString(3),
-                        resultSet.getString(4),
-                        resultSet.getString(5),
-                        resultSet.getString(6),
-                        resultSet.getString(7),
-                        resultSet.getString(8),
-                        resultSet.getString(9),
-                        resultSet.getFloat(10),
-                        resultSet.getString(11),
-                        resultSet.getString(12),
-                        resultSet.getFloat(13),
-                        resultSet.getInt(14),
-                        resultSet.getTimestamp(15),
-                        resultSet.getTimestamp(16)
-                );
-                laptopModels.add(laptopModel);
-            }
-            return laptopModels;
-        } catch (Exception e) {
-            System.out.println("Loi khi tim kiem bang price" + e);
-        }
-        return null;
-    }
-    public List<LaptopModel> findAllByMakerAndSSD(String maker, String gb) {
-        try {
-            List<LaptopModel> laptopModels = new ArrayList<>();
-            Statement statement = getConnections().createStatement();
-            String sql = "SELECT * FROM laptop WHERE maker = '" + maker + "'" + "AND ( ssd = '" + gb + "' " + "OR hdd = '"+ gb + "'" +") ";
-            ResultSet resultSet = statement.executeQuery(sql);
-            while (resultSet.next()) {
-                LaptopModel laptopModel = new LaptopModel(
-                        resultSet.getInt(1),
-                        resultSet.getString(2),
-                        resultSet.getString(3),
-                        resultSet.getString(4),
-                        resultSet.getString(5),
-                        resultSet.getString(6),
-                        resultSet.getString(7),
-                        resultSet.getString(8),
-                        resultSet.getString(9),
-                        resultSet.getFloat(10),
-                        resultSet.getString(11),
-                        resultSet.getString(12),
-                        resultSet.getFloat(13),
-                        resultSet.getInt(14),
-                        resultSet.getTimestamp(15),
-                        resultSet.getTimestamp(16)
-                );
-                laptopModels.add(laptopModel);
-            }
-            return laptopModels;
-        } catch (Exception e) {
-            System.out.println("loi khi tim kiem theo o cung va maker" + e);
-        }
-        return null;
-    }
-    public List<LaptopModel> findAllByScreensize(String Screensize){
-        try {
-            List<LaptopModel> laptopModels = new ArrayList<>();
-            Statement statement = getConnections().createStatement();
-            String sql = "SELECT * FROM laptop WHERE screen_size = '" + Screensize + "' ";
-            ResultSet resultSet = statement.executeQuery(sql);
-            while (resultSet.next()) {
-                LaptopModel laptopModel = new LaptopModel(
-                        resultSet.getInt(1),
-                        resultSet.getString(2),
-                        resultSet.getString(3),
-                        resultSet.getString(4),
-                        resultSet.getString(5),
-                        resultSet.getString(6),
-                        resultSet.getString(7),
-                        resultSet.getString(8),
-                        resultSet.getString(9),
-                        resultSet.getFloat(10),
-                        resultSet.getString(11),
-                        resultSet.getString(12),
-                        resultSet.getFloat(13),
-                        resultSet.getInt(14),
-                        resultSet.getTimestamp(15),
-                        resultSet.getTimestamp(16)
-                );
-                laptopModels.add(laptopModel);
-            }
-            return laptopModels;
-        } catch (Exception e) {
-            System.out.println("Lỗi khi tìm kiếm theo screenSize" + e);
-        }
-        return null;
-    }
-    public List<LaptopModel> findAllByRam (String ram){
-        try {
-            List<LaptopModel> laptopModels = new ArrayList<>();
-            Statement statement = getConnections().createStatement();
-            String sql = "SELECT * FROM laptop WHERE ram = '" + ram + "' ";
-            ResultSet resultSet = statement.executeQuery(sql);
-            while (resultSet.next()) {
-                LaptopModel laptopModel = new LaptopModel(
-                        resultSet.getInt(1),
-                        resultSet.getString(2),
-                        resultSet.getString(3),
-                        resultSet.getString(4),
-                        resultSet.getString(5),
-                        resultSet.getString(6),
-                        resultSet.getString(7),
-                        resultSet.getString(8),
-                        resultSet.getString(9),
-                        resultSet.getFloat(10),
-                        resultSet.getString(11),
-                        resultSet.getString(12),
-                        resultSet.getFloat(13),
-                        resultSet.getInt(14),
-                        resultSet.getTimestamp(15),
-                        resultSet.getTimestamp(16)
-                );
-                laptopModels.add(laptopModel);
-            }
-            return laptopModels;
-        } catch (Exception e) {
-            System.out.println("Lỗi khi tìm kiếm theo ram" + e);
-        }
-        return null;
-    }
-    public List <LaptopModel> findAllByCPU (String CPU){
-        try {
-            List<LaptopModel> laptopModels = new ArrayList<>();
-            Statement statement = getConnections().createStatement();
-            String sql = "SELECT * FROM laptop WHERE cpu = '" + CPU + "' ";
-            ResultSet resultSet = statement.executeQuery(sql);
-            while (resultSet.next()) {
-                LaptopModel laptopModel = new LaptopModel(
-                        resultSet.getInt(1),
-                        resultSet.getString(2),
-                        resultSet.getString(3),
-                        resultSet.getString(4),
-                        resultSet.getString(5),
-                        resultSet.getString(6),
-                        resultSet.getString(7),
-                        resultSet.getString(8),
-                        resultSet.getString(9),
-                        resultSet.getFloat(10),
-                        resultSet.getString(11),
-                        resultSet.getString(12),
-                        resultSet.getFloat(13),
-                        resultSet.getInt(14),
-                        resultSet.getTimestamp(15),
-                        resultSet.getTimestamp(16)
-                );
-                laptopModels.add(laptopModel);
-            }
-            return laptopModels;
-        } catch (Exception e) {
-            System.out.println("Lỗi khi tìm kiếm theo CPU " + e);
-        }
-        return null;
-    }
-    public List<LaptopModel> findAllByType(String Type){
-        try {
-            List<LaptopModel> laptopModels = new ArrayList<>();
-            Statement statement = getConnections().createStatement();
-            String sql = "SELECT * FROM laptop WHERE type = '" + Type + "' ";
-            ResultSet resultSet = statement.executeQuery(sql);
-            while (resultSet.next()) {
-                LaptopModel laptopModel = new LaptopModel(
-                        resultSet.getInt(1),
-                        resultSet.getString(2),
-                        resultSet.getString(3),
-                        resultSet.getString(4),
-                        resultSet.getString(5),
-                        resultSet.getString(6),
-                        resultSet.getString(7),
-                        resultSet.getString(8),
-                        resultSet.getString(9),
-                        resultSet.getFloat(10),
-                        resultSet.getString(11),
-                        resultSet.getString(12),
-                        resultSet.getFloat(13),
-                        resultSet.getInt(14),
-                        resultSet.getTimestamp(15),
-                        resultSet.getTimestamp(16)
-                );
-                laptopModels.add(laptopModel);
-            }
-            return laptopModels;
-        } catch (Exception e) {
-            System.out.println("Lỗi khi tìm kiếm theo type " + e);
-        }
-        return null;
-    }
-    public List<LaptopModel> findAllByCard(String Card){
-        try {
-            List<LaptopModel> laptopModels = new ArrayList<>();
-            Statement statement = getConnections().createStatement();
-            String sql = "SELECT * FROM laptop WHERE card = '" + Card + "' ";
-            ResultSet resultSet = statement.executeQuery(sql);
-            while (resultSet.next()) {
-                LaptopModel laptopModel = new LaptopModel(
-                        resultSet.getInt(1),
-                        resultSet.getString(2),
-                        resultSet.getString(3),
-                        resultSet.getString(4),
-                        resultSet.getString(5),
-                        resultSet.getString(6),
-                        resultSet.getString(7),
-                        resultSet.getString(8),
-                        resultSet.getString(9),
-                        resultSet.getFloat(10),
-                        resultSet.getString(11),
-                        resultSet.getString(12),
-                        resultSet.getFloat(13),
-                        resultSet.getInt(14),
-                        resultSet.getTimestamp(15),
-                        resultSet.getTimestamp(16)
-                );
-                laptopModels.add(laptopModel);
-            }
-            return laptopModels;
-        } catch (Exception e) {
-            System.out.println("Lỗi khi tìm kiếm theo card " + e);
-        }
-        return null;
-    }
-    List <LaptopModel> findAllBySold(){
-        try {
-            List<LaptopModel> laptopModels = new ArrayList<>();
-            Statement statement = getConnections().createStatement();
-            String sql = "SELECT * FROM laptop ORDER BY `sold` DESC ";
-            ResultSet resultSet = statement.executeQuery(sql);
-            while (resultSet.next()) {
-                LaptopModel laptopModel = new LaptopModel(
-                        resultSet.getInt(1),
-                        resultSet.getString(2),
-                        resultSet.getString(3),
-                        resultSet.getString(4),
-                        resultSet.getString(5),
-                        resultSet.getString(6),
-                        resultSet.getString(7),
-                        resultSet.getString(8),
-                        resultSet.getString(9),
-                        resultSet.getFloat(10),
-                        resultSet.getString(11),
-                        resultSet.getString(12),
-                        resultSet.getFloat(13),
-                        resultSet.getInt(14),
-                        resultSet.getTimestamp(15),
-                        resultSet.getTimestamp(16)
-                );
-                laptopModels.add(laptopModel);
-            }
-            return laptopModels;
-        } catch (Exception e) {
-            System.out.println("Lỗi khi tìm kiếm theo sold " + e);
-        }
-        return null;
+        return queryDB(sql);
     }
 
+    public List<LaptopModel> queryDB(String sql) {
+        try {
+            List<LaptopModel> laptopModels = new ArrayList<>();
+            Statement statement = getConnections().createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+            while (resultSet.next()) {
+                LaptopModel laptopModel = new LaptopModel(
+                        resultSet.getInt(1),
+                        resultSet.getString(2),
+                        resultSet.getString(3),
+                        resultSet.getString(4),
+                        resultSet.getString(5),
+                        resultSet.getString(6),
+                        resultSet.getString(7),
+                        resultSet.getString(8),
+                        resultSet.getString(9),
+                        resultSet.getFloat(10),
+                        resultSet.getString(11),
+                        resultSet.getString(12),
+                        resultSet.getFloat(13),
+                        resultSet.getInt(14),
+                        resultSet.getTimestamp(15),
+                        resultSet.getTimestamp(16)
+                );
+                laptopModels.add(laptopModel);
+            }
+            return laptopModels;
+        } catch (Exception e) {
+            System.out.println("Lỗi ------  " + e);
+        }
+        return null;
+    }
 }
